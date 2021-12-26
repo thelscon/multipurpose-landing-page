e9020757f27c8b23a48393a475b063d7db6ad605
thelscon <thelscon@gmail.com>
2021-12-26 20:42:48 +0200

    add hero__section block

--- script/script.js
+++ script/script.js
@@ -1,17 +1,17 @@
 'use strict' ;
 
-let header__choiсeOfStyle = document.getElementById ( 'header__choiсe-of-style' ) ;
+let hero__header__choiсeOfStyle = document.getElementById ( 'hero__header__choiсe-of-style' ) ;
 if ( window.innerWidth >= 1440 ) {
-    header__choiсeOfStyle.style.backgroundImage = 'url(./images/3.svg)' ;
+    hero__header__choiсeOfStyle.style.backgroundImage = 'url(./images/3.svg)' ;
 }
 else {
-    header__choiсeOfStyle.style.backgroundImage = 'url(./images/1.svg)' ;
+    hero__header__choiсeOfStyle.style.backgroundImage = 'url(./images/1.svg)' ;
 }
 window.addEventListener ( 'resize' , () => {
     if ( window.innerWidth >= 1440 ) {
-        header__choiсeOfStyle.style.backgroundImage = 'url(./images/3.svg)' ;
+        hero__header__choiсeOfStyle.style.backgroundImage = 'url(./images/3.svg)' ;
     }
     else {
-        header__choiсeOfStyle.style.backgroundImage = 'url(./images/1.svg)' ;
+        hero__header__choiсeOfStyle.style.backgroundImage = 'url(./images/1.svg)' ;
     }
 } ) ;
